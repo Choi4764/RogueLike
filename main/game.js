3a1cc2cf7a5e3871766e3feb79de10b527bcad8b
Bzarre <32144764@dankook.ac.kr>
2024-08-27 22:34:31 +0900

    final??

--- main/game.js
+++ main/game.js
@@ -5,57 +5,67 @@ class Player {
   constructor(hp, dmg, rech, actch, ptch) {
     this.hp = hp;
     this.dmg = dmg;
-    this.rech = 67;
-    this.actch = 33;
+    this.rech = 33;
+    this.actch = 67;
     this.ptch = 50;
     this.deal = Math.round((Math.random() * (Math.round((this.dmg * 1.7)) - this.dmg) + this.dmg));
   }
 
   reward() {
     let random = Math.round(Math.random() * 100);
-    this.hp += random;
 
-    if(random >= 50)
-        this.dmg += Math.round(this.dmg / 2);
-        this.rech += Math.round((100 - this.rech) / 10);
-        this.actch += Math.round((100 - this.actch) / 20);
-        this.ptch += Math.round((100 - this.ptch) / 50);
+    if (random >= 75) {
+      console.log(chalk.cyanBright('능력치 상승!'))
+      this.hp += random;
+      this.dmg += Math.round(Math.random() * this.dmg);
+      this.rech += Math.round(Math.random() * 3);
+      this.actch += Math.round(Math.random() * 6);
+      this.ptch += Math.round(Math.random() * 5);
     }
-  
-
-
-attack(target) {    // 플레이어의 공격
-  target.hp -= this.deal;
-}
-
-chain(target) {  //연속공격
-  if (this.rech > Math.random() * 100) {
-    this.temp += this.deal;
-    this.chain(target);
   }
-  target.hp -= this.temp;
-}
 
-drain(target) { //흡혈
-  this.temp = this.deal;
-  this.heal = Math.round(this.temp * 0.6);
 
-  target.hp -= this.temp;
-  this.hp += this.heal;
-}
 
-react(target) { //반격
-  if (this.actch > Math.random() * 100) {
-    this.temp = this.deal * 2;
+  attack(target) {    // 플레이어의 공격
+    this.deal = Math.round((Math.random() * (Math.round((this.dmg * 1.7)) - this.dmg) + this.dmg));
+    target.hp -= this.deal;
+  }
+
+  chain(target) {  //연속공격
+    this.deal = Math.round((Math.random() * (Math.round((this.dmg * 1.7)) - this.dmg) + this.dmg));
+    while (this.count <= 10) {
+      if (this.rech >= Math.random() * 100) {
+        this.count++;
+      } else
+        break;
+    }
+
+    this.temp = this.count * this.deal;
     target.hp -= this.temp;
   }
-}
 
-passthrough(target) {  //돌파
-  if (this.ptch > Math.random() * 100) {
-    target.hp = 0;
+  drain(target) { //흡혈
+    this.deal = Math.round((Math.random() * (Math.round((this.dmg * 1.7)) - this.dmg) + this.dmg));
+    this.heal = Math.round(this.deal * 0.6);
+
+    target.hp -= this.deal;
+    this.hp += this.heal;
   }
-}
+
+  react(target) { //반격
+    this.deal = Math.round((Math.random() * (Math.round((this.dmg * 1.7)) - this.dmg) + this.dmg));
+    this.temp = 0;
+    if (this.actch >= Math.random() * 100) {
+      this.temp = this.deal * 2;
+      target.hp -= this.temp;
+    }
+  }
+
+  passthrough(target) {  //돌파
+    if (this.ptch >= Math.random() * 100) {
+      target.hp = 0;
+    }
+  }
 }
 
 class Monster {
@@ -98,8 +108,8 @@ const battle = async (stage, player, monster) => {
 
     console.log(
       chalk.green(
-        `\n1.공격 2.연속공격(${player.rech}%) 3.흡혈공격(DMG의 60%) 4.반격(${player.actch}%) 5.퇴각(${player.ptch}%)`,
-      ) + chalk.redBright(`\n4, 5는 몬스터가 먼저 행동합니다!`),
+        `\n1.공격 2.연속공격(${player.rech}%) 3.흡혈공격(DMG의 60%) 4.반격(${player.actch}%) 5.돌파(${player.ptch}%)`
+      )
     );
     const choice = readlineSync.question('선택: ');
 
@@ -115,8 +125,9 @@ const battle = async (stage, player, monster) => {
         break;
       case '2':
         player.temp = 0;
+        player.count = 0;
         player.chain(monster);
-        logs.push(chalk.blue(`플레이어의 연속공격! 총 ${player.temp}의 데미지!`));
+        logs.push(chalk.blue(`플레이어의 ${player.count}회 연속공격! 총 ${player.temp}의 데미지!`));
         monster.attack(player);
         logs.push(chalk.red(`몬스터의 공격! ${monster.deal}의 데미지!`));
         break;
@@ -127,15 +138,18 @@ const battle = async (stage, player, monster) => {
         logs.push(chalk.red(`몬스터의 공격! ${monster.deal}의 데미지!`));
         break;
       case '4':
-        monster.attack(player);
-        logs.push(chalk.red(`몬스터의 공격! ${monster.deal}의 데미지!`));
         player.react(monster);
-        logs.push(chalk.blue(`플레이어의 반격! ${player.temp}의 데미지!`));
+        if (player.temp === 0) {
+          monster.attack(player);
+          logs.push(chalk.red(`몬스터의 공격! ${monster.deal}의 데미지!`));
+        } else
+          logs.push(chalk.blue(`플레이어의 반격! ${player.temp}의 데미지!`));
         break;
       case '5':
         monster.attack(player);
         logs.push(chalk.red(`몬스터의 공격! ${monster.deal}의 데미지!`));
-        player.passthrough(monster);
+        if (player.hp > 0)
+          player.passthrough(monster);
         break;
     }
   }
@@ -148,13 +162,14 @@ export async function startGame() {
   let stage = 1;
 
   while (stage <= 10) {
-    const monster = new Monster(stage * 100, stage * 5);
+    const monster = new Monster(stage * 100 + Math.round(Math.random() * stage) * 25, stage * 5 + Math.round(Math.random() * stage));
     await battle(stage, player, monster);
 
     // 스테이지 클리어 및 게임 종료 조건
     if (monster.hp <= 0) {
-      console.log(chalk.cyan('몬스터를 돌파했습니다'))
+      console.log(chalk.cyan('전투가 종료되었습니다'))
       player.reward();
+      player.hp += 50;
       readlineSync.question('입력하여 계속...')
       stage++;
     } else if (player.hp <= 0) {
